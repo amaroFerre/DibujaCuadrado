eb615bccc4810a9e25655a33dbb9cdb660853e70
amaroFerre <33329763+amaroFerre@users.noreply.github.com>
2018-08-31 19:15:09 +0200

    Update Ejercicio4.java

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -3,64 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio4;
+package ejercicio4adaboletin3;
 
+// Importo el paquete java.util.* para poder utilizar la clase Scanner
 import java.util.*;
 
 /**
  *
- * @author SYSTEM
+ * @author Amarico
  */
-public class Ejercicio4 {
+public class Ejercicio4AdaBoletin3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner entrada= new Scanner (System.in);
-        
-        int numero1;
-        
-        int numero2;
-        
-        int numeroMenor;
-        
-        int numeroMayor = 0;
-        
-        int media ;
-        
-       
-        
-       do {
-           System.out.println("Introduce un número positivo: ");
-        
-        numero1 = entrada.nextInt();
-        
-        System.out.println("Introduce un segundo número positivo: ");
-        
-        numero2 = entrada.nextInt();
-        
-        media = (numero1 / numero2);
-           
-           
-           if (numero1 == numero2){
-            
-        
-           System.out.println("Los números son iguales, introduce dos números distintos");
-           }
-           
-           if (numero1 > numero2){
-               
-               numeroMayor = numero1;
-           }else
-               numeroMenor = numero2;
-         
-           
-           
-           
-           
-       } while (numero1 == numero2);
+
+        // Creo el objeto entrada
+        Scanner entrada = new Scanner(System.in);
+
+        // Creo dos variables de tipo entero
+        int lado1;
+
+        // Pido por teclado el  lado
+        System.out.println("Introduce el  lado");
+
+        lado1 = entrada.nextInt();
+
+        // Creo el bucle
+        for (int i = 1; i <= lado1; i++) {
+
+            for (int j = 1; j <= lado1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println("");
+
+        }
+
     }
-    
+
 }
